3ae5e18934b6138f091ca48e21fdd3e79fec0e50
Nov11 <529079634@qq.com>
2019-03-10 16:23:42 +0800

    should have used simpleinboundhandler / release bytebuf

--- netty-basics/src/main/java/http/SimpleHttpServer/ServerHandler.java
+++ netty-basics/src/main/java/http/SimpleHttpServer/ServerHandler.java
@@ -1,19 +1,18 @@
 package http.SimpleHttpServer;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 
-public class ServerHandler extends ChannelInboundHandlerAdapter {
+public class ServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     private static Logger logger = LoggerFactory.getLogger(ServerHandler.class);
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest fullHttpRequest) throws Exception {
         String content = fullHttpRequest.content().toString();
         logger.info("get from client : {}", content);
         FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
