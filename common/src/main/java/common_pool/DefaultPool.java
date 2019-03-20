838f3bc1e83a9de09276c298c4a70d7ae7b2b0ad
Nov11 <529079634@qq.com>
2019-03-20 16:21:08 +0800

    objpool

--- common/src/main/java/common_pool/DefaultPool.java
+++ common/src/main/java/common_pool/DefaultPool.java
@@ -2,111 +2,128 @@ package common_pool;
 
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.PooledObjectFactory;
-import org.apache.commons.pool2.PooledObjectState;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 
-import java.io.PrintWriter;
-import java.util.Deque;
-
 public class DefaultPool {
-    static class PO implements PooledObject<Integer> {
-        int value = 0;
-        public PO() {
-        }
+//    static class PO implements PooledObject<Integer> {
+//        private int value = 0;
+//        private PooledObjectState state = PooledObjectState.IDLE;
+//
+//        public PO(int value) {
+//            this.value = value;
+//        }
+//
+//        @Override
+//        public Integer getObject() {
+//            return value;
+//        }
+//
+//        @Override
+//        public long getCreateTime() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public long getActiveTimeMillis() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public long getIdleTimeMillis() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public long getLastBorrowTime() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public long getLastReturnTime() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public long getLastUsedTime() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public int compareTo(PooledObject<Integer> pooledObject) {
+//            return 0;
+//        }
+//
+//        @Override
+//        public boolean startEvictionTest() {
+//            state = PooledObjectState.EVICTION;
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean endEvictionTest(Deque<PooledObject<Integer>> deque) {
+//            return false;
+//        }
+//
+//        @Override
+//        public boolean allocate() {
+//            state = PooledObjectState.ALLOCATED;
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean deallocate() {
+//            state = PooledObjectState.IDLE;
+//            return true;
+//        }
+//
+//        @Override
+//        public void invalidate() {
+//            value = 1;
+//            state = PooledObjectState.INVALID;
+//        }
+//
+//        @Override
+//        public void setLogAbandoned(boolean b) {
+//
+//        }
+//
+//        @Override
+//        public void use() {
+//
+//        }
+//
+//        @Override
+//        public void printStackTrace(PrintWriter printWriter) {
+//
+//        }
+//
+//        @Override
+//        public PooledObjectState getState() {
+//            return state;
+//        }
+//
+//        @Override
+//        public void markAbandoned() {
+//            state = PooledObjectState.ABANDONED;
+//        }
+//
+//        @Override
+//        public void markReturning() {
+//            state = PooledObjectState.RETURNING;
+//        }
+//    }
 
-        @Override
-        public Integer getObject() {
-            return null;
-        }
 
-        @Override
-        public long getCreateTime() {
-            return 0;
-        }
-
-        @Override
-        public long getActiveTimeMillis() {
-            return 0;
-        }
-
-        @Override
-        public long getIdleTimeMillis() {
-            return 0;
-        }
-
-        @Override
-        public long getLastBorrowTime() {
-            return 0;
-        }
-
-        @Override
-        public long getLastReturnTime() {
-            return 0;
-        }
-
-        @Override
-        public long getLastUsedTime() {
-            return 0;
-        }
-
-        @Override
-        public int compareTo(PooledObject<Integer> pooledObject) {
-            return 0;
-        }
-
-        @Override
-        public boolean startEvictionTest() {
-            return false;
-        }
-
-        @Override
-        public boolean endEvictionTest(Deque<PooledObject<Integer>> deque) {
-            return false;
-        }
-
-        @Override
-        public boolean allocate() {
-            return false;
-        }
-
-        @Override
-        public boolean deallocate() {
-            return false;
-        }
-
-        @Override
-        public void invalidate() {
-
-        }
-
-        @Override
-        public void setLogAbandoned(boolean b) {
-
-        }
-
-        @Override
-        public void use() {
-
-        }
-
-        @Override
-        public void printStackTrace(PrintWriter printWriter) {
-
-        }
-
-        @Override
-        public PooledObjectState getState() {
-            return null;
-        }
-
-        @Override
-        public void markAbandoned() {
-
-        }
-
-        @Override
-        public void markReturning() {
-
+    static class PO extends DefaultPooledObject<Integer> {
+        /**
+         * Create a new instance that wraps the provided object so that the pool can
+         * track the state of the pooled object.
+         *
+         * @param object The object to wrap
+         */
+        public PO(Integer object) {
+            super(object);
         }
     }
 
@@ -115,7 +132,7 @@ public class DefaultPool {
 
         @Override
         public PooledObject<Integer> makeObject() throws Exception {
-            return new PO();
+            return new PO(value++);
         }
 
         @Override
@@ -140,7 +157,14 @@ public class DefaultPool {
     }
 
 
-    public static void main(String[] args) {
-//        GenericObjectPool<Integer> genericObjectPool = new GenericObjectPool<>();
+    public static void main(String[] args) throws Exception {
+        GenericObjectPool<Integer> genericObjectPool = new GenericObjectPool<>(new PF());
+        Integer i1 = genericObjectPool.borrowObject();
+        genericObjectPool.returnObject(i1);
+        Integer i2 = genericObjectPool.borrowObject();
+        Integer i3 = genericObjectPool.borrowObject();
+        System.out.println(i1.hashCode());
+        System.out.println(i2.hashCode());
+        System.out.println(i3.hashCode());
     }
 }
---
+++ common/src/main/java/spring_mymatis_config/spring_mybatis_config.java
@@ -0,0 +1,4 @@
+package spring_mymatis_config;
+
+public class spring_mybatis_config {
+}
