e19365d3547aa707fa8b691e8a035e0675872acc
Nov11 <529079634@qq.com>
2019-04-06 14:11:40 +0800

    dumb examples

---
+++ common/src/main/java/network/srv_not_accepting/Server.java
@@ -0,0 +1,19 @@
+package network.srv_not_accepting;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(60000);
+        logger.info("socket created");
+        while (true) {
+            Thread.sleep(10000);
+        }
+    }
+}
