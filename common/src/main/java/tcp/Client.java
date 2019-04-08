0b6947db9c1c8ec09b00309227e028f2647d2278
Nov11 <529079634@qq.com>
2019-04-08 15:44:34 +0800

    backlog limited will produce some exception during connection

--- common/src/main/java/tcp/Client.java
+++ common/src/main/java/tcp/Client.java
@@ -3,11 +3,31 @@ package tcp;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket();
-        socket.connect(new InetSocketAddress("localhost", 17000));
-        System.out.println("exit");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Runnable job = new Runnable() {
+            @Override
+            public void run() {
+                Socket socket = new Socket();
+                try {
+                    socket.connect(new InetSocketAddress("localhost", 17000));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("exit");
+            }
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 4; i++) {
+            executorService.submit(job);
+        }
+
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+        executorService.shutdownNow();
     }
 }
