40c13ac01ee816c5286cfa16c2c97cc4f0f7382b
Nov11 <529079634@qq.com>
2019-06-25 14:47:44 +0800

    test

---
+++ common/src/test/java/tteesstt/ShowException.java
@@ -0,0 +1,17 @@
+package tteesstt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShowException {
+
+    @Test
+    public void name() {
+        Assert.fail("name");
+    }
+
+    @Test
+    public void name2() {
+        Assert.fail("name2");
+    }
+}
