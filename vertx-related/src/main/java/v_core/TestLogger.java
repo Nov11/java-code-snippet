1d827a5c203e26b774102e56bb5ae3b158fabead
Nov11 <529079634@qq.com>
2019-07-02 18:04:51 +0800

    vertx

---
+++ vertx-related/src/main/java/v_core/TestLogger.java
@@ -0,0 +1,11 @@
+package v_core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger {
+    private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
+    public static void main(String[] args) {
+        logger.info("lal");
+    }
+}
