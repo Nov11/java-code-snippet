16b96681c619063d23ba6fce07c3e4bdbd17b8d8
Nov11 <529079634@qq.com>
2019-08-23 12:15:20 +0800

    jackson serialize back and forth

--- jackson-test/src/main/java/pkg/Run.java
+++ jackson-test/src/main/java/pkg/Run.java
@@ -3,19 +3,24 @@ package pkg;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 public class Run {
     private static final Logger logger = LoggerFactory.getLogger(Run.class);
-    public static void main(String[] args) throws JsonProcessingException {
+
+    public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         Target target = new Target();
+        String s = objectMapper.writeValueAsString(target);
+        logger.info("{}", s);
+        Target t2 = objectMapper.readValue(s, Target.class);
 
-        logger.info("{}", objectMapper.writeValueAsString(target));
+        logger.info("{}", target.equals(t2));
     }
 }
--- jackson-test/src/main/java/pkg/Target.java
+++ jackson-test/src/main/java/pkg/Target.java
@@ -1,9 +1,25 @@
 package pkg;
 
+import java.util.Objects;
+
 public class Target {
     public int a;
     private int b;
 
     private Integer integer;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Target target = (Target) o;
+        return a == target.a &&
+                b == target.b &&
+                Objects.equals(integer, target.integer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, integer);
+    }
 }
