b32e1cc64f38103d7c7ad6ac838ffa6c5d6d9ca8
Nov11 <529079634@qq.com>
2019-12-26 16:47:23 +0800

    jedis timeout && where does set jetty status code 500

--- embedded-jetty/src/main/java/pkg/util/ServerUtil.java
+++ embedded-jetty/src/main/java/pkg/util/ServerUtil.java
@@ -3,6 +3,7 @@ package pkg.util;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.Slf4jRequestLog;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import javax.servlet.Servlet;
@@ -24,5 +25,15 @@ public class ServerUtil {
 
     public static Server makeServer(Class<? extends Servlet> servletClass) {
         return makeServer(1026, servletClass);
+    }
+
+    public static void configLogger(Server server) {
+        Slf4jRequestLog requestLog = new Slf4jRequestLog();
+        requestLog.setLogServer(true);
+        requestLog.setExtended(true);
+        requestLog.setLogLatency(true);
+        requestLog.setPreferProxiedForAddress(true);
+        requestLog.setLogTimeZone("GMT+08:00");
+        server.setRequestLog(requestLog);
     }
 }
