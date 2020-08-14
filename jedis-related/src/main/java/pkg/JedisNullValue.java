e47ccd90a0afd97fd284f25f89b3dc2812e928df
Nov11 <529079634@qq.com>
2020-08-14 19:33:29 +0800

    save

--- jedis-related/src/main/java/pkg/JedisNullValue.java
+++ jedis-related/src/main/java/pkg/JedisNullValue.java
@@ -6,5 +6,7 @@ public class JedisNullValue {
     public static void main(String[] args) {
         Jedis jedis = new Jedis();
         jedis.set("k".getBytes(), new byte[0]);
+        byte[] bytes = jedis.get("k".getBytes());
+        System.out.println(bytes.length);
     }
 }
