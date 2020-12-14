76c5309d802d2c8583dc3abaa3513f0d7ace9f11
Nov11 <529079634@qq.com>
2020-12-14 10:45:42 +0800

    save

--- scala-code/src/main/scala/pkg/scala_list.scala
+++ scala-code/src/main/scala/pkg/scala_list.scala
@@ -4,8 +4,9 @@ import scala.collection.mutable
 
 object scala_list {
   def main(args: Array[String]): Unit = {
-    val result: mutable.MutableList[(String, String)] = mutable.MutableList[(String, String)]()
-//    result.add()
+    val result: mutable.MutableList[(String, String)] = new mutable.MutableList[(String, String)]()
+    //result.add(("1", "2"))
     result.+=(("1", "2"))
+    print(result)
   }
 }
